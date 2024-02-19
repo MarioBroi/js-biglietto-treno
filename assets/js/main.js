df1284c510ea8d87d0697fec23ff65d69469943c
MarioBroi <inoix16@gmail.com>
2024-02-19 13:09:20 +0000

    set km and age variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log("Test");
+//console.log("Test"); 
+
+//- Chiedo all'utente quanti chilometri percorrerá il passeggero
+let km = prompt("Chilometri da percorrere");
+console.log(Number(km));//km da percorrere
+
+//- Chiedo all'utente l'etá del passeggero
+let age = prompt("Etá del passeggero");
+console.log(Number(age));//etá del passeggero
